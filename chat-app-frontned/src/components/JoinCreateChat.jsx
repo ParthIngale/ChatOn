4aa833813d6dd086d185fbe45797351aa39dbc35
ParthIngale <parthkingale23@gmail.com>
2025-08-14 13:19:20 +0530

    ok11215

--- chat-app-frontned/src/components/JoinCreateChat.jsx
+++ chat-app-frontned/src/components/JoinCreateChat.jsx
@@ -110,7 +110,8 @@ const createRoomHandler = async () => {
       {/* Animated Background Elements */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute -top-40 -right-40 w-80 h-80 bg-cyan-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse"></div>
-        <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse animation-delay-2000"></div>
+        {/* <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse animation-delay-2000"></div> */}
+       <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse" style={{ animationDelay: '2s' }}></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-60 h-60 bg-green-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse animation-delay-4000"></div>
       </div>
 
@@ -272,22 +273,22 @@ const createRoomHandler = async () => {
           </div>
         </div>
       </div>
-<style>{`
+{/* <style>{`
   .animation-delay-2000 {
     animation-delay: 2s;
   }
   .animation-delay-4000 {
     animation-delay: 4s;
   }
-`}</style>
-      {/* <style jsx>{`
+`}</style> */}
+      <style jsx>{`
         .animation-delay-2000 {
           animation-delay: 2s;
         }
         .animation-delay-4000 {
           animation-delay: 4s;
         }
-      `}</style> */}
+      `}</style>
     </div>
   );
 };
